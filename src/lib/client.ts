6b7be9932f6e4da1575f6accfe1cd53a0ce01168
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-12-18 22:54:33 +0530

    debugging...

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -4,6 +4,6 @@ import type {App} from "../app/api/[[...slugs]]/route";
 
 //this require .api to enter /api prefix 
 
-export const client = treaty<App>('localhost:3000').api;
+export const client = treaty<App>('https://securechat-blue.vercel.app/').api;
 
 
